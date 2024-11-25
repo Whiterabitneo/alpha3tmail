2a01783d09d1db1bbcb4f3d1770ac0133a8d1f85
Whiterabitneo <daily.wise.inspiration@gmail.com>
2024-11-25 06:55:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def run_script():
     return "Script executed successfully"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
